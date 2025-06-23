6a4b768408bd96f56732bf1199fb154519078e57
manta726 <sabenihfermanta@gmail.com>
2025-06-23 12:23:27 +0700

    Update __init__.py

--- auth/__init__.py
+++ auth/__init__.py
@@ -2,5 +2,6 @@
 Authentication package for LDB Application
 """
 from .auth_manager import AuthManager
+from .legacy_auth import logout, login
 
-__all__ = ['AuthManager']
+__all__ = ['AuthManager', 'logout', 'login']
